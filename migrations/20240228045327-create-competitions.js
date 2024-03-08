dd7dfcca51775ff3a3e9abcbf703d700185955d4
ScaferuZ <endrafruz@gmail.com>
2024-03-08 07:36:26 +0700

    fix: changing data types for description and createdAt

--- migrations/20240228045327-create-competitions.js
+++ migrations/20240228045327-create-competitions.js
@@ -1,46 +1,46 @@
-'use strict';
+"use strict";
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('competitions', {
+    await queryInterface.createTable("competitions", {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       title: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       category: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       description: {
-        type: Sequelize.STRING
+        type: Sequelize.LONGTEXT,
       },
       registration_fee: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       prize: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       registration_deadline: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       image: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATEONLY,
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
-      }
+        type: Sequelize.DATE,
+      },
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('competitions');
-  }
+    await queryInterface.dropTable("competitions");
+  },
 };
